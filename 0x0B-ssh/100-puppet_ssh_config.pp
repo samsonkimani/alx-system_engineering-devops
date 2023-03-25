1506b190b2b40e15e16c90e668825a9f532afb05
samsonkimani <samsonkimani43@gmail.com>
2023-03-25 16:13:43 +0300

    corrections for puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,16 @@
-#creating a pupper script to ensure to create config file and append data
-file {'/home/samson/.ssh/config':
-  ensure  => present,
-  content => '
-	Host *
-		IdentityFile ~/.ssh/school
-		PasswordAuthentication no
-	',
+# Seting up my client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
