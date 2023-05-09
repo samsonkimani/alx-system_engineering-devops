4ed3d08d3ce1bd8b4a1e78f27e8600803f7e52b0
samsonkimani <samsonkimani43@gmail.com>
2023-05-09 13:55:07 +0300

    getting the number of users in a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""model - working with reddit api"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """ a function to get the number of subscribers of a subreddit"""
+    headers = {"User-Agent": 'my_bot/0.0.1'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
