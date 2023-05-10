0b8dbccf646ce120496b7f6de5919b8931a213fd
samsonkimani <samsonkimani43@gmail.com>
2023-05-10 14:29:25 +0300

    creating the dashboard and getting the dashboard id

---
+++ 0x18-webstack_monitoring/check_datadog_host_exists.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Get the total number of active hosts returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.hosts_api import HostsApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = HostsApi(api_client)
+    response = api_instance.get_host_totals()
+
+    print(response)
