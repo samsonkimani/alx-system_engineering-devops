14123dc512d8a78cfd1f01d1731c8f9a37345019
samsonkimani <samsonkimani43@gmail.com>
2023-05-18 21:55:42 +0300

    correction on increasing the hard and soft file limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
-# increase hard file limit for holberton user
+# increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-#Increase soft file limit for user holberton
+# increase soft file limit for Holberton user.
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
